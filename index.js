0adc5d6192c877c3e054ced87e4104bccf480b5d
Ben Reeves <benwolverine2019@gmail.com>
2017-02-03 16:43:42 -0500

    Fixed express server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-config = require('./config.js');
+var express = require('express');
+var app = express();
 
-console.log(config['SlackIntegrationKey']);
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send("Hello World!");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
