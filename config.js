9e57b9b08d47014c9b6e735f285a0206da5801b1
Ben Reeves <benwolverine2019@gmail.com>
2017-02-08 13:21:01 -0500

    Added to the EECS 376 Slack

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ const CONFIG = {
 		{
 			"name": "bgreeves.slack.com",
 			"token": "EiPRlmb0WgMxvwUdO9H0LJN8"
+		},
+		{
+			"name": "eecs376w17.slack.com",
+			"token": "2SmfRbrXFPEAPtKcCMaTw84E"
 		}
 	]
 };
